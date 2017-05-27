b5895d409b880fed7209830911b0f2f51b41c886
xdv12 <xaavi1204@gmail.com>
2017-05-27 18:41:03 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ document.addEventListener("keydown", myFunction);
 
 function myFunction(e) {
     alert(e.keyCode)
-    var johncena=document.getElementById("p2").style.backgroundColor = "blue";
+    var johncena=document.getElementById("johncena").style.backgroundColor = "blue";
 }
